f3bf88dd9a3964bc9f6be5b2d652a85a08424235
najin5cfish <river.name2.najin@gmail.com>
2023-05-02 14:22:33 +0900

    test dir commit

---
+++ tmp.cpp
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <iostream>
+
+int main(void){
+  //private
+  int ready_;
+  int charge_;
+  int end_;
+  int error_;
+  
+  // --- | READY | CHARGE | END | ERROR | E1 | E2 | E3
+  uint8_t send=0b00001010;
+  for (int i=0;i<8; i++){
+  }
+  return 0;
+}
